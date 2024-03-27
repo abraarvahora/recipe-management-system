d28b8ec131c82aacef12c11f174fc663202d440f
Abraar Vahora <abraar@iizuka.co.uk>
2024-03-27 14:22:37 +0000

    Initial project setup

---
+++ WorkExperienceRecipe/scripts/app.js
@@ -0,0 +1,18 @@
+
+//create variable of recipes
+var recipes = 
+	[	
+		{
+			"id":1,
+			"name":"Chocolate Cake",
+			"ingredients":"40g of Flour\n25g of Chocolate",
+			"directions":"Step 1: Mix flour and chocolate in a bowl\nStep 2: Bake for 1 hour at 350 degrees celcius",
+			"notes":"Try using vanilla esscence next time"
+		},
+	
+	];
+
+//document.ready
+$(function() {
+	console.log(recipes);
+})
